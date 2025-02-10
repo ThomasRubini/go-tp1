03b7fc10a4e8b00fc63ca39f3c185a2e77888a37
Thomas Rubini <74205383+ThomasRubini@users.noreply.github.com>
2025-02-10 16:30:40 +0100

    add linked_list.go

---
+++ linked_list/linked_list.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+)
+
+type LinkedList[T any] struct {
+	data T
+	next *LinkedList[T]
+}
+
+func NewLinkedList[T any](data ...T) *LinkedList[T] {
+	l := &LinkedList[T]{}
+	for _, d := range data {
+		l.Append(d)
+	}
+	return l
+}
+
+func (l *LinkedList[T]) Append(data T) {
+	if l.next == nil {
+		l.next = &LinkedList[T]{data: data}
+	} else {
+		l.next.Append(data)
+	}
+}
+
+func (l *LinkedList[T]) String() string {
+	if l == nil {
+		return ""
+	}
+	sb := ""
+	currentNode := l
+	for currentNode.next != nil {
+		sb += fmt.Sprintf(", %v", currentNode.data)
+		currentNode = currentNode.next
+	}
+	return fmt.Sprintf("LinkedList(%v)", sb[2:])
+}
+
+func (l *LinkedList[T]) Length() int {
+	if l == nil {
+		return 0
+	}
+	return 1 + l.next.Length()
+}
+
+func main() {
+	l := NewLinkedList(1, 2, 3, 4, 5)
+	fmt.Println(l)
+
+}
